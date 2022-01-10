7fd99003bc7d97c218ea5961a89136f1a25427d4
Thien Nguyen <97245070+thien1013@users.noreply.github.com>
2022-01-10 11:11:51 -0700

    signin complete

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -4,6 +4,7 @@ import React, { Component } from "react";
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
 import { signInWithGoogle } from "../../firebase/firebase.util";
+import { auth } from "../../firebase/firebase.util";
 
 export default class SignIn extends Component {
   constructor() {
@@ -14,8 +15,18 @@ export default class SignIn extends Component {
     };
   }
 
-  handleSubmit = (event) => {
+  handleSubmit = async (event) => {
     event.preventDefault();
+    const { email, password } = this.state;
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({
+        email: "",
+        password: "",
+      });
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   handleChange = (event) => {
