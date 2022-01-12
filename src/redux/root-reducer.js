443e2dab3e892669e8dfba2d47c596f970dacdb6
Thien Nguyen <97245070+thien1013@users.noreply.github.com>
2022-01-11 20:51:16 -0700

    persist data

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,8 +1,19 @@
 import userReducer from "./user/user-reducer";
 import { combineReducers } from "redux";
 import cartReducer from "./cart/cart.reducer";
+import { persistReducer } from "redux-persist";
 
-export default combineReducers({
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["cart"],
+};
+
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
 });
+
+export default persistReducer(persistConfig, rootReducer);
