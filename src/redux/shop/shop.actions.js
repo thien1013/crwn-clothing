694c952d8ec19b11645ac486a28c6ba369f7666e
Thien Nguyen <97245070+thien1013@users.noreply.github.com>
2022-01-17 10:02:36 -0700

    bug fix

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,8 +1,39 @@
 import ShopActionTypes from "./shop.types";
+import {
+  firestore,
+  convertCollectionsSnapshotToMap,
+} from "../../firebase/firebase.util";
 
-export const updateCollections = (collectionsMap) => {
+export const fetchCollectionsStart = () => {
   return {
-    type: ShopActionTypes.UPDATE_COLLECTIONS,
+    type: ShopActionTypes.FETCH_COLLECTIONS_START,
+  };
+};
+
+export const fetchCollectionsStartAsync = () => {
+  return (dispatch) => {
+    const collectionRef = firestore.collection("collections");
+    dispatch(fetchCollectionsStart());
+    collectionRef
+      .get()
+      .then((snapshot) => {
+        const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+        dispatch(fetchCollectionsSuccess(collectionsMap));
+      })
+      .catch((error) => dispatch(fetchCollectionsFailure(error.message)));
+  };
+};
+
+export const fetchCollectionsSuccess = (collectionsMap) => {
+  return {
+    type: ShopActionTypes.FETCH_COLLECTIONS_SUCCESS,
     payload: collectionsMap,
+  };
+};
+
+export const fetchCollectionsFailure = (errorMessage) => {
+  return {
+    type: ShopActionTypes.FETCH_COLLECTIONS_FAILURE,
+    payload: errorMessage,
   };
 };
